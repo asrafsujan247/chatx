43681cfaf3731cb4d700f81d21ef2c32800b9e49
asrafsujan247 <asraf.sujan247@gmail.com>
2025-11-14 17:35:02 +0600

    Implemented message routes functionality

---
+++ chatx-backend/src/controllers/message.controller.js
@@ -0,0 +1,103 @@
+import cloudinary from "../lib/cloudinary.js";
+import Message from "../models/Message.js";
+import User from "../models/User.js";
+
+// Get all contacts except the logged-in user
+export const getAllContacts = async (req, res) => {
+  try {
+    // Exclude the logged-in user from the contacts list
+    const loggedInUserId = req.user._id;
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select(-"password");
+
+    // Return the filtered list of users
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.log("Error in getAllContacts:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// Get messages by user id
+export const getMessagesByUserId = async (req, res) => {
+  try {
+    // get my id and user to chat id
+    const myId = req.user._id;
+    const { id: userToChatId } = req.params;
+    // fetch messages between the two users
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    });
+    // return messages
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessagesByUserId:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// Send message to user by id
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+    let imageUrl;
+    if (image) {
+      // upload base64 image to cloudinary
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+    // create new message
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+    await newMessage.save();
+
+    // todo: send message in real-time using socket.io
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in sendMessage:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+// getChatPartners
+export const getChatPartners = async (req, res) => {
+  try {
+    const loggedInUser = req.user._id;
+    // find the messages where the logged-in user is either the sender or receiver
+    const messages = await Message.find({
+      $or: [{ senderId: loggedInUser }, { receiverId: loggedInUser }],
+    });
+    // extract unique chat partner ids
+    const chatPartnerIds = [
+      ...new Set(
+        messages.map((msg) =>
+          msg.senderId.toString() === loggedInUser.toString()
+            ? msg.receiverId.toString()
+            : msg.senderId.toString()
+        )
+      ),
+    ];
+
+    // fetch user details of chat partners
+    const chatPartners = await User.find({
+      _id: { $in: chatPartnerIds },
+    }).select("-password");
+
+    // return chat partners
+    res.status(200).json(chatPartners);
+  } catch (error) {
+    console.log("Error in getChatPartners:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
--- chatx-backend/src/routes/message.route.js
+++ chatx-backend/src/routes/message.route.js
@@ -1,9 +1,27 @@
 import express from "express";
+import {
+  getAllContacts,
+  getChatPartners,
+  getMessagesByUserId,
+  sendMessage,
+} from "../controllers/message.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
-router.get("/send", (req, res) => {
-  res.send("Send message endpoint");
-});
+// the middlewares execute in order - so requests get rate-limited first, then authenticated
+// this is actually more efficient since unauthenticated requests get blocked by rate limiting before hitting the auth middleware.
+
+router.use(arcjetProtection, protectRoute);
+
+// Get all contacts
+router.get("/contacts", getAllContacts);
+// Get chat partners
+router.get("/chats", getChatPartners);
+// get messages by user id
+router.get("/:id", getMessagesByUserId);
+// send message to user by id
+router.post("/send/:id", sendMessage);
 
 export default router;
