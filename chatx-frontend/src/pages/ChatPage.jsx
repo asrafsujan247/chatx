a7d3a996bc2f02c030e977bf988cbdb5bdfc2b8e
asrafsujan247 <asraf.sujan247@gmail.com>
2025-11-19 00:11:31 +0600

    Implemented login page with adding login and logout method

--- chatx-frontend/src/pages/ChatPage.jsx
+++ chatx-frontend/src/pages/ChatPage.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { useAuthStore } from "../store/useAuthStore";
 
 const ChatPage = () => {
- 
+  const { logout } = useAuthStore();
 
   return (
     <div>
-      <h1>chat page</h1>
+      <h1 className="text-blue-300 mb-2 text-3xl">chat page</h1>
+      <button className="btn btn-secondary z-50 relative" onClick={logout}>
+        Logout
+      </button>
     </div>
   );
 };
